308e60c0f05a82e6dfba7a3a38074a96b2725a65
Lena <eseliver@icloud.com>
2022-07-13 21:12:04 +0300

    01-1

--- task_01.py
+++ task_01.py
@@ -3,18 +3,10 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-# number_N = int(input("Введите вещественное число: ")) # не понимаю, почему не работает аналогия решения на С# :(
-# result = 0
-# while number_N > 0:
-#     digit = number_N % 10
-#     result += digit
-#     number_N = number_N / 10
-# print('%.0f' % result)
-
-number_N = input("Введите вещественное число: ")
+number_N = int(input("Введите вещественное число: "))
 result = 0
-
-for i in number_N:
-    if i.isdigit():
-        result = result+int(i)
+while number_N > 0:
+    digit = number_N % 10
+    result += digit
+    number_N = number_N // 10
 print(result)
